aa2f6ad7dd7f36f98c4fb7d17cd96b4942c4f153
jkworks <info@jilliankayworks.com>
2021-09-20 14:07:11 -0600

    category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,83 +3,81 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // try {
-  //   const libraryCardData = await LibraryCard.findAll({
-  //     include: [{ model: Reader }],
-  //   });
-  //   res.status(200).json(libraryCardData);
-  // } catch (err) {
-  //   res.status(500).json(err);
-  // }
+router.get('/', async (req, res) => {
+   try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   
-  // try {
-  //   const libraryCardData = await LibraryCard.findByPk(req.params.id, {
-  //     include: [{ model: Reader }],
-  //   });
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Category }],
+    });
 
-  //   if (!libraryCardData) {
-  //     res.status(404).json({ message: 'No library card found with that id!' });
-  //     return;
-  //   }
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category available with that id!' });
+      return;
+    }
 
-  //   res.status(200).json(libraryCardData);
-  // } catch (err) {
-  //   res.status(500).json(err);
-  // }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // try {
-  //   const locationData = await LibraryCard.create({
-  //     reader_id: req.body.reader_id,
-  //   });
-  //   res.status(200).json(locationData);
-  // } catch (err) {
-  //   res.status(400).json(err);
-  // }
-  // create a new category
+//create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(
+      req.body
+    );
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
-  // try {
-  //   const userData = await User.update(req.body, {
-  //     where: {
-  //       id: req.params.id,
-  //     },
-  //   });
-  //   if (!userData[0]) {
-  //     res.status(404).json({ message: 'No user with this id!' });
-  //     return;
-  //   }
-  //   res.status(200).json(userData);
-  // } catch (err) {
-  //   res.status(500).json(err);
-  // }
-  // update a category by its `id` value
+   try {
+     const categoryData = await User.update(req.body, {
+       where: {
+         id: req.params.id,
+       },
+     });
+     if (!categoryData[0]) {
+       res.status(404).json({ message: 'No category with this id!' });
+       return;
+     }
+     res.status(200).json(categoryData);
+   } catch (err) {
+     res.status(500).json(err);
+   }
+ // update a category by its `id` value
 });
 
 router.delete('/:id', async (req, res) => {
-  // try {
-  //   const locationData = await Location.destroy({
-  //     where: {
-  //       id: req.params.id
-  //     }
-  //   });
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
 
-  //   if (!locationData) {
-  //     res.status(404).json({ message: 'No location found with this id!' });
-  //     return;
-  //   }
+    if (!categoryData) {
+      res.status(404).json({ message: 'No Category found with this id!' });
+      return;
+    }
 
-  //   res.status(200).json(locationData);
-  // } catch (err) {
-  //   res.status(500).json(err);
-  // }
-  // delete a category by its `id` value
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+ // delete a category by its `id` value
 });
 
 module.exports = router;
