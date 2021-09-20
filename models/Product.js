e9bd18b5d791bd48ac926cb99c712dc41d77ca22
jkworks <info@jilliankayworks.com>
2021-09-20 13:21:42 -0600

    Products model w validation

--- models/Product.js
+++ models/Product.js
@@ -9,24 +9,24 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // Driver.hasOne(License, {
-    //   foreignKey: 'driver_id',
-    //   onDelete: 'CASCADE',
-    // });
-    
-    // License.belongsTo(Driver, {
-    //   foreignKey: 'driver_id',
-    // });
-    
-    // Driver.hasMany(Car, {
-    //   foreignKey: 'driver_id',
-    //   onDelete: 'CASCADE',
-    // });
-    
-    // Car.belongsTo(Driver, {
-    //   foreignKey: 'driver_id',
-    // });
     // define columns
+    product_id: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      autoIncrement: true
+         },
+
+      product_name: {
+      type: DataTypes.STRING,
+          allowNull: false
+             },
+      product_price: {
+      type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            isDecimal: true
+          }
+             },
   },
   {
     sequelize,
